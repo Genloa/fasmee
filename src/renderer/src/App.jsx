9b5757185d2490e021134fc07813de75991b2add
Genesis L. Delgado H. <98361965+Genloa@users.noreply.github.com>
2024-10-21 16:24:15 -0400

    realizacion del login

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home/home'
 import Usuarios from './pages/usuarios/Usuarios'
+import Login from './pages/auth/Login'
 
 function App() {
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login/>} />
       <Route path="/dash/home" element={<Home />} />
       <Route path="/dash/users" element={<Usuarios />} />
     </Routes>
