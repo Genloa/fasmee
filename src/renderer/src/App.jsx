0d27e1c7879b401121dd244c00655d734160603b
David A. Alvarado C <davicii711@gmail.com>
2024-12-16 10:35:47 -0400

    refactor: optimizar importaciones en App.jsx y eliminar duplicados

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './hooks/useAuth'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 import Login from './pages/auth/Login'
+import Citas from './pages/citas/Citas'
 import Connection from './pages/connection/Connection'
 import Home from './pages/home/home'
 import Pacientes from './pages/pacientes/Pacientes'
 import Usuarios from './pages/usuarios/Usuarios'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import Citas from './pages/citas/Citas'
+
 function App() {
   return (
     <AuthProvider>
