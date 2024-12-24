5c8f8cb9bd4cd14c18167ae7a9c58777da8c58b2
David A. Alvarado C <davicii711@gmail.com>
2024-12-23 22:10:36 -0400

    refactor: implementar transacciones en la función updateUserRol para mejorar la gestión de errores

--- src/main/modules/usuarios/usecases/updateUserRol.js
+++ src/main/modules/usuarios/usecases/updateUserRol.js
@@ -1,32 +1,43 @@
 import { ipcMain } from 'electron'
+import db from '../../../singletons/database/database'
 import { Perfil, Rol, RolOnPerfil, Usuario } from '../../../singletons/database/schema'
 
 ipcMain.handle('updateUserRol', async (event, { perfilId, rolId }) => {
+  const t = await db.getConnection().transaction()
+
   try {
     if (!perfilId || !rolId) {
       throw new Error('perfilId o rolId no pueden ser nulos.')
     }
 
     // Crear la relación entre el perfil y el rol
-    await RolOnPerfil.create({
-      rolId: rolId,
-      perfilId: perfilId
-    })
+    await RolOnPerfil.create(
+      {
+        rolId: rolId,
+        perfilId: perfilId
+      },
+      { transaction: t }
+    )
 
     // Buscar el perfil y sus asociaciones
-    const perfil = await Perfil.findOne({
-      where: { id: perfilId },
-      include: [
-        {
-          model: Usuario,
-          as: 'usuario'
-        },
-        {
-          model: Rol,
-          as: 'roles'
-        }
-      ]
-    })
+    const perfil = await Perfil.findOne(
+      {
+        where: { id: perfilId },
+        include: [
+          {
+            model: Usuario,
+            as: 'usuario'
+          },
+          {
+            model: Rol,
+            as: 'roles'
+          }
+        ]
+      },
+      { transaction: t }
+    )
+
+    await t.commit()
 
     // Verificar que el perfil fue encontrado
     if (!perfil) {
@@ -45,6 +56,7 @@ ipcMain.handle('updateUserRol', async (event, { perfilId, rolId }) => {
     // Retornar el objeto usuario completo
     return usuario
   } catch (error) {
+    await t.rollback()
     console.error('Error creando el rol del usuario:', error.message)
     throw error
   }
