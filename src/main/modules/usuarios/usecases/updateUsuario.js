80dc292b7cceb99f80a9842c5b86f28a9e6f0f3e
David A. Alvarado C <davicii711@gmail.com>
2024-12-23 21:48:35 -0400

    refactor: mejorar la función updateUsuario para manejar transacciones y errores

--- src/main/modules/usuarios/usecases/updateUsuario.js
+++ src/main/modules/usuarios/usecases/updateUsuario.js
@@ -1,52 +1,74 @@
 import { ipcMain } from 'electron'
+import db from '../../../singletons/database/database'
 import { Departamento, Perfil, Rol, Usuario } from '../../../singletons/database/schema'
 import hashPassword from '../../../utils/hashPassword'
 
 ipcMain.handle('updateUsuario', async (event, { id, data }) => {
-  data.password = await hashPassword(data.password)
+  const t = await db.getConnection().transaction()
 
-  const usuario = await Usuario.findByPk(id)
+  try {
+    data.password = await hashPassword(data.password)
 
-  usuario.username = data.username
-  usuario.password = data.password
-
-  await usuario.save()
-
-  const perfil = await Perfil.findOne({
-    where: { usuarioId: usuario.id },
-    include: [
-      {
-        model: Rol,
-        as: 'roles'
-      },
-      {
-        model: Departamento,
-        as: 'departamento'
-      }
-    ]
-  })
-
-  if (!perfil) {
-    throw new Error('Perfil no encontrado.')
-  }
-
-  perfil.nombres = data.nombres
-  perfil.apellidos = data.apellidos
-  perfil.fecha_nacimiento = new Date(data.fechaNacimiento)
-  perfil.tipo_cedula = data.tipoCedula
-  perfil.cedula = data.cedula
-  perfil.correo = data.correo
-  perfil.telefono = data.telefono
-  perfil.departamentoId = data.departamentoId
-
-  await perfil.save()
-
-  return {
-    ...usuario.toJSON(),
-    perfil: {
-      ...perfil.toJSON(),
-      roles: perfil.roles.map((rol) => rol.toJSON()),
-      departamento: perfil.departamento.toJSON()
+    const usuario = await Usuario.findByPk(id)
+    if (!usuario) {
+      throw new Error('Usuario no encontrado.')
     }
+
+    await usuario.update(
+      {
+        username: data.username,
+        password: data.password
+      },
+      { transaction: t }
+    )
+
+    const perfil = await Perfil.findOne(
+      {
+        where: { usuarioId: usuario.id },
+        include: [
+          {
+            model: Rol,
+            as: 'roles'
+          },
+          {
+            model: Departamento,
+            as: 'departamento'
+          }
+        ]
+      },
+      { transaction: t }
+    )
+
+    if (!perfil) {
+      throw new Error('Perfil no encontrado.')
+    }
+
+    await perfil.update(
+      {
+        nombres: data.nombres,
+        apellidos: data.apellidos,
+        fecha_nacimiento: new Date(data.fechaNacimiento),
+        tipo_cedula: data.tipoCedula,
+        cedula: data.cedula,
+        correo: data.correo,
+        telefono: data.telefono,
+        departamentoId: data.departamentoId
+      },
+      { transaction: t }
+    )
+
+    await t.commit()
+
+    return {
+      ...usuario.toJSON(),
+      perfil: {
+        ...perfil.toJSON(),
+        roles: perfil.roles.map((rol) => rol.toJSON()),
+        departamento: perfil.departamento.toJSON()
+      }
+    }
+  } catch (error) {
+    console.error('Error en updateUsuario:', error)
+    throw error
   }
 })
