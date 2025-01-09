855c3c8c4c2364471336ba0f6a899750effd6c59
David A. Alvarado C. <davicii711@gmail.com>
2025-01-08 21:14:45 -0400

    feat: agregar rutas protegidas para las páginas de la aplicación

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,21 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './hooks/useAuth'
 
-import Login from './pages/auth/Login'
-import Citas from './pages/citas/Citas'
-import Connection from './pages/connection/Connection'
-import Home from './pages/home/Home'
-import Pacientes from './pages/pacientes/Pacientes'
-import Usuarios from './pages/usuarios/Usuarios'
-import ColaPacientes from './pages/colaPacientes/ColaPacientes'
-import AtenderPaciente from './pages/atenderPaciente/AtenderPaciente'
-import Inventario from './pages/Inventario/Inventario'
-import Cronograma from './pages/cronograma/Cronograma'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import Servicios from './pages/servicios/Servicios'
+import ProtectedRoute from './middleware/ProtectedRoute'
+import AtenderPaciente from './pages/atenderPaciente/AtenderPaciente'
+import Login from './pages/auth/Login'
+import Citas from './pages/citas/Citas'
+import ColaPacientes from './pages/colaPacientes/ColaPacientes'
+import Connection from './pages/connection/Connection'
+import Cronograma from './pages/cronograma/Cronograma'
 import Historias from './pages/historias/Historias'
+import Home from './pages/home/Home'
+import Inventario from './pages/Inventario/Inventario'
+import Pacientes from './pages/pacientes/Pacientes'
+import Servicios from './pages/servicios/Servicios'
+import Usuarios from './pages/usuarios/Usuarios'
 
 function App() {
   return (
@@ -23,20 +24,90 @@ function App() {
       <Routes>
         <Route path="/" element={<Connection />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dash/users" element={<Usuarios />} />
-        <Route path="/dash/pacientes" element={<Pacientes />} />
-        <Route path="/dash/citas" element={<Citas />} />
-        <Route path="/dash/ColaPacientes" element={<ColaPacientes />} />
-        <Route path="/dash/atenderPaciente" element={<AtenderPaciente />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/users"
+          element={
+            <ProtectedRoute>
+              <Usuarios />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/pacientes"
+          element={
+            <ProtectedRoute>
+              <Pacientes />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/citas"
+          element={
+            <ProtectedRoute>
+              <Citas />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/ColaPacientes"
+          element={
+            <ProtectedRoute>
+              <ColaPacientes />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/atenderPaciente"
+          element={
+            <ProtectedRoute>
+              <AtenderPaciente />
+            </ProtectedRoute>
+          }
+        />
         {/*  <Route
           path="/dash/atenderPaciente/:departamentoId/:pacienteId"
           element={<AtenderPaciente />}
         />*/}
-        <Route path="/dash/inventario" element={<Inventario />} />
-        <Route path="/dash/cronograma" element={<Cronograma />} />
-        <Route path="/dash/servicios" element={<Servicios />} />
-        <Route path="/dash/historias" element={<Historias />} />
+        <Route
+          path="/dash/inventario"
+          element={
+            <ProtectedRoute>
+              <Inventario />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/cronograma"
+          element={
+            <ProtectedRoute>
+              <Cronograma />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/servicios"
+          element={
+            <ProtectedRoute>
+              <Servicios />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dash/historias"
+          element={
+            <ProtectedRoute>
+              <Historias />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </AuthProvider>
   )
