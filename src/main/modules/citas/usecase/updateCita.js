b89dad9a680b555cb049c1dc16aa7c7ca3b7d152
David A. Alvarado C. <davicii711@gmail.com>
2025-01-11 08:34:32 -0400

    feat: agregar manejo de transacciones en la creación, actualización y eliminación de citas

--- src/main/modules/citas/usecase/updateCita.js
+++ src/main/modules/citas/usecase/updateCita.js
@@ -1,35 +1,54 @@
 import { ipcMain } from 'electron'
+import db from '../../../singletons/database/database'
 import { Cita, Perfil } from '../../../singletons/database/schema' // Asegúrate de importar todos los modelos necesarios
 
 ipcMain.handle('updateCita', async (event, { id, data }) => {
+  const t = await db.getConnection().transaction()
+
   try {
-    console.log('Updating cita:----------------', id, data)
-    const cita = await Cita.findByPk(id)
+    const cita = await Cita.findOne(
+      {
+        where: { id: id }
+      },
+      { transaction: t }
+    )
+
     if (!cita) {
       throw new Error('Cita not found')
     }
 
-    await cita.update({
-      fecha_cita: new Date(data.fechaCita),
-      perfilId: data.medicoId,
-      pacienteId: data.pacienteId,
-      departamentoId: data.departamentoId
-    })
+    await cita.update(
+      {
+        fecha_cita: new Date(data.fechaCita),
+        perfilId: data.medicoId,
+        pacienteId: data.pacienteId,
+        departamentoId: data.departamentoId
+      },
+      { transaction: t }
+    )
 
-    const citasPaciente = await Perfil.findOne({
-      include: [
-        {
-          model: Cita,
-          as: 'citasSolicitadas' // Alias de la asociación
+    const perfil = await Perfil.findOne(
+      {
+        include: [
+          {
+            model: Cita,
+            as: 'citasSolicitadas' // Alias de la asociación
+          }
+        ],
+        where: {
+          id: cita.pacienteId
         }
-      ],
-      where: {
-        id: cita.pacienteId
-      }
-    })
-    const pacientesSerializables = citasPaciente.toJSON()
-    return pacientesSerializables
+      },
+      { transaction: t }
+    )
+
+    const perfilSerializado = perfil.toJSON()
+
+    await t.commit()
+
+    return perfilSerializado
   } catch (error) {
+    await t.rollback()
     console.error('Error updating cita:', error)
     throw error
   }
